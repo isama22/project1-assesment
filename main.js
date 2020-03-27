fae19b490543bc30b4d7965b6f92cc2836270b9c
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-03-27 15:15:07 -0700

    input field value appends to html

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 //cached----------------------------------------------------------------
 let plusButton = document.getElementById('plus-button');
 //event listeners---------------------------------------------------
-plusButton.addEventListener("click", buttonWorks);
+plusButton.addEventListener("click", appendPTag);
 //functions----------------------------------------------------------
 function buttonWorks(){
     console.log('hello');
+}
+
+function appendPTag () {
+    var x = document.getElementById('input-field').value;
+    document.getElementById("p-tag").innerHTML = x;
 }
