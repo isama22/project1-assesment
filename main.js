46cd98a3a7cc4acbebb2cc8bea1ff1f31e69c149
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-03-27 15:09:58 -0700

    plus button logs test hello

---
+++ main.js
@@ -0,0 +1,8 @@
+//cached----------------------------------------------------------------
+let plusButton = document.getElementById('plus-button');
+//event listeners---------------------------------------------------
+plusButton.addEventListener("click", buttonWorks);
+//functions----------------------------------------------------------
+function buttonWorks(){
+    console.log('hello');
+}
