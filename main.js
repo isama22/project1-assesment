062658943cc645d72d043a34cf234f60877336b9
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-04-02 02:56:42 -0700

    fixed

--- main.js
+++ main.js
@@ -1,41 +1,57 @@
-// realized after writing all this that i got too focused on making the new input value appear that i wasn't thinking about making it actually do any addition, so when you write a new input and press the plus button it only appends itself, but it works great as an input value appender! spent the rest of my time wrestling with css but scrapping it and pushing to gh-pages since i didnt have time to start over 
+//set variables to null and 1 for input field load default
+//show input and counter number w variables/show what they are/template literals/attatch values
+//function for add/take input field value and store it in a variable/ use that to add/sub/parse/attatch
+//function for subtract
 
-//cached----------------------------------------------------------------
-let plusButton = document.getElementById('plus-button');
-let minusButton = document.getElementById('minus-button');
+//
+let displayNum = 0;
+let userNum = 1;
+
+let showNum = document.getElementById('pTag');
+let showInput = document.getElementById('inputField');
+
+showNum.textContent = `${displayNum}`;
+showInput.value = `${userNum}`;
+
+//-------
+let plusButton = document.getElementById('plusButton');
+let minusButton = document.getElementById('minusButton');
 //event listeners---------------------------------------------------
-plusButton.addEventListener("click", appendPTag);
-minusButton.addEventListener("click", subtractInputValue);
-//functions----------------------------------------------------------
+plusButton.addEventListener('click', addInputValue);
+minusButton.addEventListener('click', subtractInputValue);
+//-----
+// function buttonWorks(){
+//     console.log('hello');
+// }
+//add function
 function buttonWorks(){
     console.log('hello');
 }
 
-function appendPTag () {
-    var x = document.getElementById('inputField').value;
-        while (p2Tag.firstChild){
-        p2Tag.removeChild(p2Tag.firstChild);
-        };
-        document.getElementById("pTag").innerHTML = x;
-}
-
-// a = document.getElementById('pTag').value;
-// b = document.getElementById('inputField').value;
-
-// c = 3;
-// d = 2;
-
-// e = "has to equal the current input value";
-// f = "has to equal the current value of pTag";
-//var minusButtonWork = e - f;
-
-function subtractInputValue() {
-    let minusButtonWork = document.getElementById('inputField').value - document.getElementById('pTag').value;
-    document.getElementById('pTag').innerHTML = minusButtonWork;
-}
-
-// var minusButtonWork = firstValue - secondValue;
-// function subtractInputValue() {
-//     let minusButtonWork = document.getElementById('inputField').value - document.getElementById('pTag').value;
-//     document.getElementById("pTag").innerHTML = minusButtonWork;
+function addInputValue(){
+//     //take current value of input field and parseInt add to current val of display num
+//     displayNum = parseInt(document.getElementById('inputField').value); + parseInt(showNum);
+//     console.log(displayNum); 
 // }
+//parseInt + 
+    let showNum = document.getElementById('pTag');
+    showNum.value;
+    userNum = document.getElementById('inputField').value;
+    displayNum = parseInt(displayNum) + parseInt(userNum);
+    document.getElementById('pTag').textContent = `${displayNum}`;
+}
+//subtract function 
+//vice versa
+function subtractInputValue() {
+    let showNum = document.getElementById('pTag');
+    showNum.value;
+    userNum = document.getElementById('inputField').value;
+    displayNum = parseInt(displayNum) - parseInt(userNum);
+    document.getElementById('pTag').textContent = `${displayNum}`;
+}
+
+
+
+
+
+
